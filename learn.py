e139130f60f93660a047a6307d5b868635fe801e
Abdulahi Saheed <abdulahisaheed001@gmail.com>
2022-08-03 11:23:57 +0100

    Added a Pythin file
    Included a few line of codes showing name, age, and height of people

---
+++ learn.py
@@ -0,0 +1,9 @@
+name = "Saheed"
+Age = 32
+Height = 1.69
+#Print My name is Saheed and I am 32 years old with a height of 1.69 cm
+print("My name is" + name + " and I am " + str(Age) + "years old with a heaight of" + str(Height) + "cm")
+
+
+
+height = ["Ola", 34, "Shade", 29, "Bolanle", 46]
